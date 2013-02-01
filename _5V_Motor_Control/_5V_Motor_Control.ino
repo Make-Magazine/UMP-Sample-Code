ebdf751633d199cb8aa55c0c6ef227daea85bc01
Eric Weinhoffer <eweinhoffer@gmail.com>
2013-02-01 15:43:26 -0800

    Reverted from controlling speed percentage back to normal 0-255

--- _5V_Motor_Control/_5V_Motor_Control.ino
+++ _5V_Motor_Control/_5V_Motor_Control.ino
@@ -9,14 +9,13 @@ Upon uploading, open the serial monitor and type in a motor speed, from 0 to 255
  
  
 int motorPin = 3;
-int newspeed;
  
 void setup() 
 { 
   pinMode(motorPin, OUTPUT);
   Serial.begin(9600);
   while (! Serial);
-  Serial.println("Speed 0 to 100%");
+  Serial.println("Speed 0 to 255");
 } 
  
  
@@ -25,10 +24,9 @@ void loop()
   if (Serial.available())
   {
     int speed = Serial.parseInt(); 
-    newspeed = map(speed, 0, 255, 0, 100); // Scales the force reading to degrees for servo control
-    if (newspeed >= 0 && newspeed <= 100) // This if statement will prevent the user from entering invalid motor speeds
+    if (speed >= 0 && speed <= 255) // This if statement will prevent the user from entering invalid motor speeds
     {
-      analogWrite(motorPin, newspeed); 
+      analogWrite(motorPin, speed); 
     }
   }
 } 
